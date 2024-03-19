fc790ba86f42f22ca1863bb7641c62d0fbe0ed49
Madhav Rawal <112850999+madhavrawal29@users.noreply.github.com>
2024-03-19 15:46:44 +0530

    winning login implemented

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,12 @@ import Player from "./Components/Player.jsx";
 import Log from "./Components/Log.jsx";
 import { WINNING_COMBINATIONS } from "./winning-combinations.js";
 
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
 function derivedActivePlayer(gameTurns) {
   let currentPlayer = "X";
 
@@ -20,6 +26,34 @@ function App() {
   // const [activePlayer, setActivePlayer] = useState("X");
 
   const activePlayer = derivedActivePlayer(gameTurns);
+
+  let gameBoard = initialGameBoard;
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+  }
+
+  let winner;
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymol =
+      gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymol =
+      gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymol =
+      gameBoard[combination[2].row][combination[2].column];
+
+    if (
+      firstSquareSymol &&
+      firstSquareSymol === secondSquareSymol &&
+      firstSquareSymol === thirdSquareSymol
+    ) {
+      winner = firstSquareSymol;
+    }
+  }
 
   function handleSelectSquare(rowIndex, colIndex) {
     // setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
@@ -50,7 +84,8 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
+        {winner && <p>You won, {winner}!</p>}
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
     </main>
